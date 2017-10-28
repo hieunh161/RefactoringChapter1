7cdf4ad9de17bd435506ac522cd6b42b57660f81
Nguyễn Huy Hiệu <hieunh161@gmail.com>
2017-10-28 20:26:34 +0900

    step1: add unit test code

--- src/main/java/vn/com/fsoft/Customer.java
+++ src/main/java/vn/com/fsoft/Customer.java
@@ -29,19 +29,19 @@ public class Customer {
 			Rental each = (Rental) rentals.nextElement();
 			// determine amounts for each line
 			switch (each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2)
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3)
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
-				break;
+				case Movie.REGULAR:
+					thisAmount += 2;
+					if (each.getDaysRented() > 2)
+						thisAmount += (each.getDaysRented() - 2) * 1.5;
+					break;
+				case Movie.NEW_RELEASE:
+					thisAmount += each.getDaysRented() * 3;
+					break;
+				case Movie.CHILDRENS:
+					thisAmount += 1.5;
+					if (each.getDaysRented() > 3)
+						thisAmount += (each.getDaysRented() - 3) * 1.5;
+					break;
 			}
 			// add frequent renter points
 			frequentRenterPoints++;
---
+++ src/test/java/vn/com/fsoft/CustomerTest.java
@@ -0,0 +1,64 @@
+package vn.com.fsoft;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+//import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.equalTo;
+import org.junit.Test;
+
+public class CustomerTest {
+	
+	private static final Movie THE_LION_KING = new Movie("The Lion King", Movie.CHILDRENS);
+	private static final Movie THE_PIRATE_5 = new Movie("The pirate", Movie.REGULAR);
+	private static final Movie SPIDER_MAN = new Movie("Spiderman", Movie.NEW_RELEASE);
+	private static final Movie INVALID_MOVIE = new Movie("Invalid", 4);
+
+	private final Customer customer = new Customer("HieuNH5");
+
+	@Test
+	public void basicNewReleaseRental() {
+		customer.addRental(new Rental(SPIDER_MAN, 1));
+		assertThat(customer.statement(), equalTo(expectedMessage("Spiderman", 3.0, 3.0, 1)));
+	}
+	
+	@Test
+	public void shouldDiscountNewReleaseRental() {
+		customer.addRental(new Rental(SPIDER_MAN, 3));
+		assertThat(customer.statement(), equalTo(expectedMessage("Spiderman", 9.0, 9.0, 2)));
+	}
+
+	@Test
+	public void basicRegularRental() {
+		customer.addRental(new Rental(THE_PIRATE_5, 1));
+		assertThat(customer.statement(), equalTo(expectedMessage("The pirate", 2.0, 2.0, 1)));
+	}
+
+	@Test
+	public void shouldDiscountReularRental() {
+		customer.addRental(new Rental(THE_PIRATE_5, 4));
+		assertThat(customer.statement(), equalTo(expectedMessage("The pirate", 5, 5, 1)));
+	}
+	
+	@Test
+	public void basicChildrenRental() {
+		customer.addRental(new Rental(THE_LION_KING, 2));
+		assertThat(customer.statement(), equalTo(expectedMessage("The Lion King", 1.5, 1.5, 1)));
+	}
+	
+	@Test
+	public void shouldDiscountChildrenRental() {
+		customer.addRental(new Rental(THE_LION_KING, 5));
+		assertThat(customer.statement(), equalTo(expectedMessage("The Lion King", 4.5, 4.5, 1)));
+	}
+	
+	@Test
+	public void invalidRental() {
+		customer.addRental(new Rental(INVALID_MOVIE, 5));
+		assertThat(customer.statement(), equalTo(expectedMessage("Invalid", 0.0, 0.0, 1)));
+	}
+
+	private String expectedMessage(String rental, double price, double amount, int frequentPoints) {
+		return String.format(
+				"Rental Record for HieuNH5\n	%s	%s\nAmount owed is %s\nYou earned %d frequent renter points",
+				rental, price, amount, frequentPoints);
+	}
+}
